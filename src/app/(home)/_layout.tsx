cb1288230c5eccb038cb88e7249679d4bea27a5f
barotdiv <divbarot15@gmail.com>
2026-08-22 15:28:56 +0530

    feat: implement authenticated home route layout and user dashboard view

--- src/app/(home)/_layout.tsx
+++ src/app/(home)/_layout.tsx
@@ -2,12 +2,28 @@ import { useAuth } from "@clerk/expo";
 import { Redirect, Stack } from "expo-router";
 
 export default function Layout() {
-    const { isSignedIn, isLoaded } = useAuth()
+    const { isSignedIn, isLoaded } = useAuth();
+
     if (!isLoaded) {
-        return null
+        return null;
     }
     if (!isSignedIn) {
-        return <Redirect href="/(auth)/sign-in" />
+        return <Redirect href="/(auth)/sign-in" />;
     }
-    return <Stack screenOptions={{ headerShown: false }} />;
+
+    return (
+        <Stack
+            screenOptions={{
+                headerShown: true,
+            }}
+        >
+            <Stack.Screen
+                name="index"
+                options={{
+                    title: "index",
+                    headerTitle: "index",
+                }}
+            />
+        </Stack>
+    );
 }
